08993ce505b3fbb1a008e62c2f23952c98c597ce
Prasadasw <prasadaswar6870@gmail.com>
2024-09-02 14:31:21 +0530

    changes

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = () => {
     <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Noteworthy technology acquisitions 2021
+          Noteworthy technology acquisitions 2024
         </h5>
       </a>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
